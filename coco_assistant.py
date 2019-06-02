5574604a52f13d2be5278a18da3d058490d5bd0b
Ashwin Nair <ashwin.nair@inceptioniai.org>
2019-06-02 13:58:35 +0400

    Add COCO to YOLO converter

--- coco_assistant.py
+++ coco_assistant.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 from pycocotools.coco import COCO
-
+from tqdm import tqdm
 
 class COCO_Assistant():
 
@@ -151,6 +151,81 @@ class COCO_Assistant():
 	    	plt.show()
 
 
+	def split(self, x, train=0.9, test=0.1, validate=0.0, shuffle=False):  # split training data
+	    n = len(x)
+	    v = np.arange(n)
+	    if shuffle:
+	        np.random.shuffle(v)
+
+	    i = round(n * train)  # train
+	    j = round(n * test) + i  # test
+	    k = round(n * validate) + j  # validate
+	    return v[:i], v[i:j], v[j:k]
+
+
+	# Convert COCO JSON file into YOLO format labels -------------------------------
+	def coco2yolo(self, name, file):
+		# Import json
+	    with open(file) as f:
+	        data = json.load(f)
+
+	    # Create folders
+	    path = 'yolo_annotations'
+	    if os.path.exists(path):
+	        shutil.rmtree(path)  # delete output folder
+	    os.makedirs(path)  # make new output folder
+	    os.makedirs(path + os.sep + 'labels')  # make new labels folder
+
+	    # Write images and shapes
+	    name = path + os.sep + name
+	    file_id, file_name, width, height = [], [], [], []
+	    for i, x in enumerate(tqdm(data['images'], desc='Files and Shapes')):
+	        file_id.append(x['id'])
+	        file_name.append('IMG_' + x['file_name'].split('IMG_')[-1])
+	        width.append(x['width'])
+	        height.append(x['height'])
+
+	        # filename
+	        with open(name + '.txt', 'a') as file:
+	            file.write('%s\n' % file_name[i])
+
+	        # shapes
+	        with open(name + '.shapes', 'a') as file:
+	            file.write('%g, %g\n' % (x['width'], x['height']))
+
+	    # Write *.names file
+	    for x in tqdm(data['categories'], desc='Names'):
+	        with open(name + '.names', 'a') as file:
+	            file.write('%s\n' % x['name'])
+
+	    # Write labels file
+	    for x in tqdm(data['annotations'], desc='Annotations'):
+	        i = file_id.index(x['image_id'])  # image index
+	        image_name = file_name[i]
+	        extension = image_name.split('.')[-1]
+	        label_name = image_name.replace(extension, 'txt')
+
+	        # The COCO bounding box format is [top left x, top left y, width, height]
+	        box = np.array(x['bbox'],dtype=np.float64)
+	        box[:2] += box[2:] / 2  # xy top-left corner to center
+	        box[[0, 2]] /= width[i]  # normalize x
+	        box[[1, 3]] /= height[i]  # normalize y
+
+	        # 'out/labels/' + label_name, 'a'
+	        with open(os.path.join(path,'labels',label_name), 'a') as file:
+	            file.write('%g %.6f %.6f %.6f %.6f\n' % (x['category_id'] - 1, *box))
+
+	    # Split data into train, test, and validate files
+	    file_name = sorted(file_name)
+	    i, j, k = self.split(file_name, train=0.9, test=0.1, validate=0.0, shuffle=False)
+	    datasets = {'train': i, 'test': j, 'val': k}
+	    for key, item in datasets.items():
+	        with open(name + '_' + key + '.txt', 'a') as file:
+	            for i in item:
+	                file.write('%s\n' % file_name[i])
+
+	    print('Done. Output saved to %s' % (os.getcwd() + os.sep + path))
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -159,6 +234,8 @@ def parse_args():
     parser.add_argument('--make',action='store_true',help='create coco data')
     parser.add_argument('--remove',action='store_true',help='remove certain categories from the annotations')
     parser.add_argument('--cat_count',action='store_true',help='generate countplot of class occurrences')
+    parser.add_argument('--to_yolo',action='store_true',help='generate yolo annotations from coco')
+
     args = parser.parse_args()
 
     return args
@@ -188,6 +265,14 @@ def main():
 		for i in ann_files:
 			coco_ann = COCO(i)
 			cas.disp_cat_count(annotations=coco_ann, name=os.path.basename(i)[:-5], bar="h", show=False)
+
+	if args.to_yolo is True:
+		name = 'yolo'  # new dataset name
+		ann_dir = os.path.join(os.getcwd(),'annotations')
+		assert os.path.exists(ann_dir) is True, "Annotations directory does not exist. Please create a directory here with all the json files you want to check"
+		file = os.path.join(ann_dir,'coco.json')  # coco json to convert
+		cas.coco2yolo(name,file)
+
 
 if __name__ == "__main__":
 	main()
